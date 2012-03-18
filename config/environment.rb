d60251f0cdbfdf11c40c6c2a3af080339dc04f13
Lefika Ngwenya <rngwenya@fnb.co.za>
2012-03-18 23:04:51 +0200

    Initial commit

---
+++ config/environment.rb
@@ -0,0 +1,5 @@
+# Load the rails application
+require File.expand_path('../application', __FILE__)
+
+# Initialize the rails application
+NGWENYARL2005142582012IT00237P04::Application.initialize!
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NGWENYARL2005142582012IT00237P04::Application.config.secret_token = '0e1374035315ffeb9f0b5a114e949b1257206017739a7d19944a0be27c7de190d4120064f0503346be7cefceb8f5019f175d750354faa6d095a8c2328b1610ab'
